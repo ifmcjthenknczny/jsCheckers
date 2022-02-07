606dabe6b80c4563557ba51e447a70017bd391e9
M <41164583+ifmcjthenknczny@users.noreply.github.com>
2022-02-07 20:10:53 +0100

    Legal moves (without chained taking pieces)

--- script.js
+++ script.js
@@ -54,31 +54,34 @@ function generateStartPosition() {
     }
 }
 
-// function pieceHeldDown () {
-//     const heldPieceSquare = this.closest('div.grid__square');
-//     heldPieceSquare.classList.add('grid__square--blackClicked');
-//     console.log(this.closest('div.grid__square'));
-// }
-
 function pieceHold() {
     pieceUnhold();
     this.setAttribute('id', 'pieceClicked');
-
-    // if ((this.classList.contains("piece--white") && lastMoveBlack) || (this.classList.contains("piece--black") && !lastMoveBlack))
+    if (orderOfTurns()) {
+        generateLegalMovesMark(legalMoves());
+    }
 }
 
 function pieceUnhold() {
     if (document.querySelector('#pieceClicked')) document.querySelector('#pieceClicked').removeAttribute('id');
+    if (document.querySelectorAll('.legalMoves')) {
+        for (element of document.querySelectorAll('.legalMove')) {
+            element.remove();
+        }
+    }
+}
+
+function nodeListContains(nodelist, obj) {
+    if (-1 < Array.from(nodelist).indexOf(obj)) return true;
+    return false;
 }
 
 function movePiece() {
     const clickedPiece = document.querySelector('#pieceClicked');
-    if (clickedPiece && !this.childNodes.length && legalMove()) {
-        // if not the same square
-        if (clickedPiece.closest('.grid__square') !== this) {
-            this.appendChild(clickedPiece);
-            lastMoveBlack = !lastMoveBlack;
-        }
+    if (clickedPiece && this.firstElementChild.classList.contains('legalMove') && orderOfTurns()) {
+        this.appendChild(clickedPiece);
+        lastMoveBlack = !lastMoveBlack;
+        pieceUnhold();
     }
 }
 
@@ -98,32 +101,106 @@ function buttonsInit() {
     });
 }
 
-function legalMove() {
+function orderOfTurns() {
     const clickedPiece = document.querySelector('#pieceClicked');
     if ((clickedPiece.classList.contains('piece--white') && lastMoveBlack) || (clickedPiece.classList.contains('piece--black') && !lastMoveBlack)) return true
     return false
 }
 
+function legalMoves() {
+    const cols = 'abcdefgh'.split('');
+    const rows = range(8, 1);
+
+    const pieceClicked = document.querySelector('#pieceClicked');
+    const pieceClickedSquare = pieceClicked.parentElement.id;
+    const pieceClickedCol = pieceClickedSquare.split('')[0];
+    const pieceClickedRow = parseInt(pieceClickedSquare.split('')[1]);
+    const isWhite = pieceClicked.classList.contains('piece--white');
+
+    const normalMoveCandidates = [];
+    const captureCandidates = [];
+    const legalMovesList = [];
+
+    const colorCoeff = isWhite ? 1 : -1;
+    if (!isWhite) rows.reverse();
+
+    // if not last row
+    if (pieceClickedRow !== rows[rows.length - 1]) {
+        if (pieceClickedCol !== 'a') normalMoveCandidates.push(`${cols[cols.indexOf(pieceClickedCol)-1]}${pieceClickedRow+colorCoeff}`);
+        if (pieceClickedCol !== 'h') normalMoveCandidates.push(`${cols[cols.indexOf(pieceClickedCol)+1]}${pieceClickedRow+colorCoeff}`);
+    }
+    if (pieceClickedCol !== 'g' && pieceClickedCol !== 'h') {
+        if (pieceClickedRow !== rows[rows.length - 2] && pieceClickedRow !== rows[rows.length - 1]) captureCandidates.push(`${cols[cols.indexOf(pieceClickedCol)+2]}${pieceClickedRow+2*colorCoeff}`);
+        if (pieceClickedRow !== rows[0] && pieceClickedRow !== rows[1]) captureCandidates.push(`${cols[cols.indexOf(pieceClickedCol)+2]}${pieceClickedRow-2*colorCoeff}`);
+    }
+    if (pieceClickedCol !== 'a' && pieceClickedCol !== 'b') {
+        if (pieceClickedRow !== rows[rows.length - 2] && pieceClickedRow !== rows[rows.length - 1]) captureCandidates.push(`${cols[cols.indexOf(pieceClickedCol)-2]}${pieceClickedRow+2*colorCoeff}`);
+        if (pieceClickedRow !== rows[0] && pieceClickedRow !== rows[1]) captureCandidates.push(`${cols[cols.indexOf(pieceClickedCol)-2]}${pieceClickedRow-2*colorCoeff}`);
+    }
+
+    //check if square is occupied by another piece
+    for (let normalMoveCandidate of normalMoveCandidates) {
+        const targetSquare = document.querySelector(`#${normalMoveCandidate}`);
+        if (!targetSquare.firstElementChild) legalMovesList.push(targetSquare);
+    }
+    //checks if there is a piece to capture and square not occupied
+    for (let captureCandidate of captureCandidates) {
+        const targetSquare = document.querySelector(`#${captureCandidate}`);
+        if (!targetSquare.firstElementChild && pieceToTake(pieceClickedSquare, captureCandidate)) legalMovesList.push(targetSquare);
+    }
+    // console.log(legalMovesList)
+    return legalMovesList
+}
+
+function pieceToTake(originalSquare, targetSquare) {
+    const cols = 'abcdefgh'.split('');
+    const rows = '12345678'.split('');
+    const [originalCol, originalRow] = originalSquare;
+    const [targetCol, targetRow] = targetSquare;
+    const squareBetween = document.querySelector(`#${cols[(cols.indexOf(originalCol)+cols.indexOf(targetCol))/2]}${(parseInt(originalRow)+parseInt(targetRow))/2}`);
+    if (squareBetween.firstElementChild) {
+        if ((squareBetween.firstElementChild.classList.contains('piece--white') && !lastMoveBlack) || (squareBetween.firstElementChild.classList.contains('piece--black') && lastMoveBlack)) return true;
+        return false
+    }
+}
+
+function generateLegalMovesMark(legalMovesList) {
+    for (legalMoveSquare of legalMovesList) {
+        const legalMoveMark = document.createElement('div');
+        legalMoveMark.classList.add('legalMove');
+        legalMoveSquare.appendChild(legalMoveMark);
+    }
+}
+
+function startGame() {
+    generateBoard();
+    generateStartPosition();
+    buttonsInit();
+}
+
 let lastMoveBlack = true;
 let playWhite = true;
-generateBoard();
-generateStartPosition();
-buttonsInit();
+startGame();
+
 
 // TO DO wygląd
+//html description
+//readme
 //fajny font
 //smooth transition moves
 //box shadow
 //tekstura drewna
 //responsywne
 //wygląd damki - pseudodiv w środku
+//wygląd bierek
 
 // TO DO logika
 //legalne ruchy
 //zbijanie i licznik zbić
-//promocja i obsługa damki
+//promocja i ruchy damki
 //random ai
 //obracanie szachownicy
 //wybór koloru pionków
 //warunki zwycięstwa
 //unhold na body
+//bicia combo jako legalny ruch
